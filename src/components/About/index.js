abdb9218bcde9a75318f378205ac9fc5768cd159
Shy <fleetwoodshybreasia@gmail.com>
2022-04-06 23:52:11 -0400

    added about and nav components

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import coverImage from '../../assets/cover/cover-image.jpeg';
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          I'm a Software Developer student at The Ohio State University. I have skills with JQuery, Javascript, Bootstrap, SASS, MySQL, MongoDB, Express, ReactJS, Node, React, and MERN. 
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
